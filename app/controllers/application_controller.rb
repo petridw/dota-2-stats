f8086a8b8a753dd85d3fb3a8510fe4202414579b
davidpetri <petridw@gmail.com>
2015-01-20 21:12:28 -0800

    Added Navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def welcome
-    redirect_to signup_path
+    if current_user
+      redirect_to home_path
+    else
+      redirect_to signup_path
+    end
   end
 
   def current_user
