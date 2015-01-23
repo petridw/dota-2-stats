311de171fb78b6245b174ab8b6efcaec156da9b1
davidpetri <petridw@gmail.com>
2015-01-23 14:39:55 -0800

    added some table styling and links

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,24 @@
 class MatchesController < ApplicationController
   def index
-    @matches = update_match_list
+    matches_json = SteamController.get_live_games
+    @matches = []
+
+    matches_json.each do |match_json|
+
+      league = League.find(match_json['league_id']) || nil
+
+      match_hash = {
+        match_id: match_json['match_id'],
+        spectators: match_json['spectators'],
+        duration: match_json['scoreboard']['duration'],
+        league: league,
+        series_type: match_json['series_type'],
+        radiant_kills: match_json['scoreboard']['radiant']['score'],
+        dire_kills: match_json['scoreboard']['dire']['score']
+      }
+      @matches.push(match_hash)
+    end
+
   end
 
   def create
@@ -11,7 +29,6 @@ class MatchesController < ApplicationController
   end
 
   def update_match_list
-    match_list = SteamController.get_live_games
-
+    match_json = SteamController.get_live_games
   end
 end
