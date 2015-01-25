03fab83869fe19f7f066c270af86ba37c152bfe4
davidpetri <petridw@gmail.com>
2015-01-25 11:03:22 -0800

    fixed redis.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
-
 end
