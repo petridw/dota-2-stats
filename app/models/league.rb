37ff0043fdd23e879a62a052bcc9734aa2279e1a
davidpetri <petridw@gmail.com>
2015-01-28 00:02:02 -0800

    fixed league last active dates

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,32 +1,44 @@
 class League
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :name, type: String
   field :url, type: String
   field :last_active, type: DateTime
 
   def self.update_leagues
-    leagues = SteamController.get_leagues
+    #only update if league data is more than 24 hours old
+    league = League.first
 
-    if leagues
-      leagues.each do |league_json|
+    if league
+      last_update = league.updated_at.to_time.to_i
+      now = DateTime.now.to_i
+    end
 
-        fixed_name = league_json['name'].split("_").drop(2).join(" ")
-        league_hash = { id: league_json['leagueid'], 
-                        name: fixed_name, 
-                        url: league_json['tournament_url']}
+    if league == nil || (now - last_update) > 86400
 
-        league_in_db = League.find(league_json['leagueid'])
+      leagues = SteamController.get_leagues
 
-        if league_in_db
-          league_in_db.update(league_hash)
-        else
-          league_hash[:last_active] = DateTime.now
-          League.create(league_hash)
+      if leagues
+        leagues.each do |league_json|
+
+          fixed_name = league_json['name'].split("_").drop(2).join(" ")
+          league_hash = { id: league_json['leagueid'], 
+                          name: fixed_name, 
+                          url: league_json['tournament_url']}
+
+          league_in_db = League.find(league_json['leagueid'].to_i)
+
+          if league_in_db
+            league_in_db.update(league_hash)
+          else
+            League.create(league_hash)
+          end
         end
-      end
-    else
-      puts "error, no leagues could be gotten"
-    end 
+      else
+        puts "error, no leagues could be gotten"
+      end 
+
+    end
       
   end
 
