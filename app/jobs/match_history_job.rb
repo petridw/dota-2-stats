59ae5be7d2907a16bd4b89d9c348c9ad790295d0
davidpetri <petridw@gmail.com>
2015-02-03 13:04:17 -0800

    put jobs inside of begin/rescue blocks.

--- app/jobs/match_history_job.rb
+++ app/jobs/match_history_job.rb
@@ -8,42 +8,46 @@ class MatchHistoryJob
     matches = []
     count = 0
 
-    while more_matches do
+    begin
+      while more_matches do
 
-      match_array = SteamController.get_match_history(steam_id, start_at)
+        match_array = SteamController.get_match_history(steam_id, start_at)
 
-      if match_array
-        matches += match_array
-      else
-        break
+        if match_array
+          matches += match_array
+        else
+          break
+        end
+
+        if match_array.count <= 1
+          more_matches = false
+        else
+          start_at = match_array[-1]['match_id']
+        end
+
       end
 
-      if match_array.count <= 1
-        more_matches = false
-      else
-        start_at = match_array[-1]['match_id']
+      matches.each do |match_json|
+
+        match = Match.find(match_json['match_id'])
+
+        if match == nil || reload
+          puts "didn't find match so about to save it"
+
+          #pass along uid so that proplayers can be added to user if found
+          MatchJob.new.perform(match_json['match_id'], reload, uid)
+
+          # error = match_json['match_id']
+          # match = Match.new(error: error)
+        else
+          puts "found match so didn't save it"
+        end
+
       end
 
     end
-
-    matches.each do |match_json|
-
-      match = Match.find(match_json['match_id'])
-
-      if match == nil || reload
-        puts "didn't find match so about to save it"
-
-        #pass along uid so that proplayers can be added to user if found
-        MatchJob.new.perform(match_json['match_id'], reload, uid)
-
-        # error = match_json['match_id']
-        # match = Match.new(error: error)
-      else
-        puts "found match so didn't save it"
-      end
-
-    end
-
+  rescue
+    puts "MATCH HISTORY FOR #{steam_id} FUCKIN ERRORED DOG"
   end
 
 end
--- app/jobs/match_job.rb
+++ app/jobs/match_job.rb
@@ -7,70 +7,74 @@ class MatchJob
 
     match_json = SteamController.get_match(match_id)
 
-    #match may have been added by another thread, so don't add it if it's already there!
-    #but, if update is true, then update this match no matter what
-    match = Match.find(match_id.to_i)
+    begin
+      #match may have been added by another thread, so don't add it if it's already there!
+      #but, if update is true, then update this match no matter what
+      match = Match.find(match_id.to_i)
 
-    if (match == nil) || update
+      if (match == nil) || update
 
-      match = Match.new if (match == nil)
-      match.players = []
+        match = Match.new if (match == nil)
+        match.players = []
 
-      match.has_pro = false
+        match.has_pro = false
 
-      players = match_json['players']
+        players = match_json['players']
 
-      players.each do |p|
+        players.each do |p|
 
-        #check if player is pro
-        pro_in_db = Proplayer.find(p['account_id'].to_i)
-        if pro_in_db
-          pro = true
-          match.has_pro = true
-          puts "adding pro player to user"
-          current_user.proplayers.push(pro_in_db) unless current_user.proplayers.find(pro_in_db.id)
-        else
-          pro = false
+          #check if player is pro
+          pro_in_db = Proplayer.find(p['account_id'].to_i)
+          if pro_in_db
+            pro = true
+            match.has_pro = true
+            puts "adding pro player to user"
+            current_user.proplayers.push(pro_in_db) unless current_user.proplayers.find(pro_in_db.id)
+          else
+            pro = false
+          end
+
+          hero_name = Hero.find(p['hero_id']).name.downcase.gsub(" ", "_")
+          match.players.push(
+            Player.new(
+              id: p['account_id'],
+              player_slot: p['player_slot'],
+              hero: hero_name,
+              item_0: p['item_0'],
+              item_1: p['item_1'],
+              item_2: p['item_2'],
+              item_3: p['item_3'],
+              item_4: p['item_4'],
+              item_5: p['item_5'],
+              kills: p['kills'],
+              deaths: p['deaths'],
+              assists: p['assists'],
+              gold: p['gold'],
+              last_hits: p['last_hits'],
+              denies: p['denies'],
+              level: p['level'],
+              gold_per_min: p['gold_per_min'],
+              xp_per_min: p['xp_per_min'],
+              hero_damage: p['hero_damage'],
+              tower_damage: p['tower_damage'],
+              hero_healing: p['hero_healing'],
+              gold_spent: p['gold_spent'],
+              pro_player: pro
+            ))
         end
 
-        hero_name = Hero.find(p['hero_id']).name.downcase.gsub(" ", "_")
-        match.players.push(
-          Player.new(
-            id: p['account_id'],
-            player_slot: p['player_slot'],
-            hero: hero_name,
-            item_0: p['item_0'],
-            item_1: p['item_1'],
-            item_2: p['item_2'],
-            item_3: p['item_3'],
-            item_4: p['item_4'],
-            item_5: p['item_5'],
-            kills: p['kills'],
-            deaths: p['deaths'],
-            assists: p['assists'],
-            gold: p['gold'],
-            last_hits: p['last_hits'],
-            denies: p['denies'],
-            level: p['level'],
-            gold_per_min: p['gold_per_min'],
-            xp_per_min: p['xp_per_min'],
-            hero_damage: p['hero_damage'],
-            tower_damage: p['tower_damage'],
-            hero_healing: p['hero_healing'],
-            gold_spent: p['gold_spent'],
-            pro_player: pro
-          ))
+        match.radiant_win = match_json['radiant_win']
+        match.duration = match_json['duration']
+        match.start_time = match_json['start_time']
+        match.id = match_json['match_id']
+
+        match.update
+
+        current_user.matches.push(match) unless current_user.matches.find(match.id)
+
       end
-
-      match.radiant_win = match_json['radiant_win']
-      match.duration = match_json['duration']
-      match.start_time = match_json['start_time']
-      match.id = match_json['match_id']
-
-      match.update
-
-      current_user.matches.push(match) unless current_user.matches.find(match.id)
-
+    rescue
+      puts "MATCH #{match_id} FUCKIN ERRORED DOG"
     end
     
   end
