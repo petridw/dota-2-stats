87f6a908ddbffa7fbba5d1314f926a5e821edc65
davidpetri <petridw@gmail.com>
2015-02-03 12:56:04 -0800

    Added output message to end up user update.

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,9 +25,18 @@ class User
   # this could take a while.....
   def self.resync_match_data
 
+    start_time = Time.now.to_i
+
     User.all.each do |u|
       MatchHistoryJob.new.perform(u.steam_id, true, u.id)
     end
+
+    end_time = Time.now.to_i
+
+    total_time = end_time - start_time
+
+    puts "User update complete!!!!! Took #{total_time} seconds!"
+
   end
 
 end
