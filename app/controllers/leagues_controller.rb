6c80a0d1cfe70f89ae7070f651ecffe37701b575
davidpetri <petridw@gmail.com>
2015-02-03 12:53:11 -0800

    added pagination to leagues list

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,5 +1,5 @@
 class LeaguesController < ApplicationController
   def index
-    @leagues = League.all.order_by(last_active: :desc)
+    @leagues = League.all.order_by(last_active: :desc).page(params[:page]).per(ITEMS_PER_PAGE)
   end
 end
