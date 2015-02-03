fc41ff441f97abfae95f6ed88a7e6ab42a70ef39
davidpetri <petridw@gmail.com>
2015-02-03 12:39:35 -0800

    added slowweb limiting init to environment.rb, since it will only load once from here

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# --- Set up constants for environment ---
+# items per page for pagination
+ITEMS_PER_PAGE = 50
+
+# limit requests to Steam to 3 requests per 1 second
+SlowWeb.limit('api.steampowered.com', 3, 1)
