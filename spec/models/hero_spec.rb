d844ccabdc7102316061d108baaee580d5c053e1
davidpetri <petridw@gmail.com>
2015-02-26 11:36:09 -0800

    add hero spec, remove item spec

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Hero, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @hero = Hero.new(name: "shadow friend")
+  end
+
+  describe ".name_upcase" do
+    it "capitalizes the first letter of each word in the name" do
+      expect(@hero.name_upcase).to eq("Shadow Friend")
+    end
+  end
+
 end
